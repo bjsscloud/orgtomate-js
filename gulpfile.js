d70b2a997a6bdeae6fb8714b439f75f14b461558
Mike Peachey <mike.peachey@bjss.com>
2021-09-16 18:14:11 +0100

    Gifs

--- gulpfile.js
+++ gulpfile.js
@@ -14,6 +14,7 @@ const gulpIf = require('gulp-if');
 const gzip = require('gulp-gzip');
 const log = require('fancy-log');
 const rename = require('gulp-rename');
+const replace = require('gulp-replace');
 const markedMan = require('gulp-marked-man');
 const sourcemaps = require('gulp-sourcemaps');
 const typedoc = require('gulp-typedoc');
@@ -23,6 +24,7 @@ const uglify = require('gulp-uglify');
 const tsProject = typescript.createProject('./src/tsconfig.json');
 
 const paths = {
+  gifs: ['src/docs/*.gif'],
   ghconfig: ['src/docs/_config.yml'],
   scripts: ['src/**/*.ts'],
   dest: 'dist',
@@ -45,14 +47,33 @@ gulp.task('ghconfig', () => {
     .pipe(gulp.dest(paths.docs))
 });
 
-gulp.task('typedoc', () => {
-  return gulp.src(paths.scripts).pipe(
-    typedoc({
-      name: 'Orgtomate',
-      tsconfig: 'src/tsconfig.json',
-      out: 'docs/',
-    }),
+gulp.task('gifs', () => {
+  return gulp.src(paths.gifs)
+    .pipe(gulp.dest(paths.docs))
+});
+
+gulp.task('index-fix', async () => {
+  /** This makes me very sad https://github.com/TypeStrong/typedoc/issues/185 */
+  await new Promise(
+    resolve => setTimeout(resolve, 5000)
   );
+  return gulp.src('docs/index.html', {base: './'})
+    .pipe(replace(/img src="docs\//g, 'img src="'))
+    .pipe(gulp.dest('.'))
+});
+
+gulp.task('typedoc', () => {
+  return new Promise((resolve, reject) => {
+    gulp.src(paths.scripts).pipe(
+      typedoc({
+        name: 'Orgtomate',
+        tsconfig: 'src/tsconfig.json',
+        out: 'docs/',
+      }),
+    )
+    .on('end', resolve)
+    .on('error', reject);
+  });
 });
 
 gulp.task('marked-man', () => {
@@ -99,7 +120,7 @@ gulp.task('compile', () => {
   // .pipe(gulpDebug({title: 'after dest:'}))
 });
 
-gulp.task('doc', gulp.series('test', 'clean-docs', gulp.parallel('typedoc', 'ghconfig')));
+gulp.task('doc', gulp.series('test', 'clean-docs', gulp.parallel('typedoc', 'ghconfig', 'gifs'), 'index-fix'));
 gulp.task('build', gulp.series('test', 'clean-dest', gulp.parallel('compile', 'marked-man')));
-gulp.task('build-all', gulp.series('test', 'clean', gulp.parallel('typedoc', 'ghconfig', 'marked-man', 'compile')));
+gulp.task('build-all', gulp.series('test', 'clean', gulp.parallel('typedoc', 'ghconfig', 'gifs', 'marked-man', 'compile'), 'index-fix'));
 gulp.task('default', gulp.series('build'));
